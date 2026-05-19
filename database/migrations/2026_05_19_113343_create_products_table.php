00d06fb9bdd2349718b89c46a1f302860ae13300
Aulya <2411102441080@umkt.ac.id>
2026-05-19 18:43:04 +0700

    feat: membuat migrasi tabel categories, stores, dan products

---
+++ database/migrations/2026_05_19_113343_create_products_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            // Menghubungkan produk ke tabel toko (Jika toko dihapus, produk ikut terhapus)
+            $table->foreignId('store_id')->constrained('stores')->onDelete('cascade');
+            // Menghubungkan produk ke tabel kategori
+            $table->foreignId('category_id')->constrained('categories')->onDelete('cascade');
+            
+            $table->string('name');
+            $table->integer('price');
+            $table->string('image')->nullable(); // Menyimpan nama file/path foto produk
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};git add .
+git commit -m "feat: membuat migrasi tabel categories, stores, dan products"
+git push origin feature-backend
